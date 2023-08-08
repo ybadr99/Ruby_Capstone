76a8793aa9a85eaa6308ea8aae96c1b1893d555e
ybadr99 <yousefbadr909@gmail.com>
2023-08-08 20:21:42 +0300

    add main file with options and make user inpute a choose

---
+++ main.rb
@@ -0,0 +1,34 @@
+require './classes/item'
+
+def options_list()
+  list = "
+        Please choose an option by enterin a number (1-7):
+        1 - List of all Books
+        2 - List of all MusicAlbums
+        6 - List of all Games
+        4 - Add book
+        3 - Add MusicAlbum
+        5 - Add game
+        7 - Exit
+        "
+  puts list
+  gets.chomp
+end
+
+def main
+  puts 'Welcome to Catalog of my things!'
+  number = options_list
+
+  loop do
+    case number
+    when '7'
+      puts 'Thank you for using this app Goodbye!'
+      break
+    else
+      puts 'Invalid input'
+      break
+    end
+  end
+end
+
+main
